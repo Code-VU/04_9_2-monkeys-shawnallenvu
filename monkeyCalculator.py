d9891676ed6a2778245b0113757244fe4bc4d231
Shawn Allen <Shawn.Allen@vu.com>
2025-03-21 12:38:42 -0500

    Added if else for checking monkey smiling states and printing status depending.

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -4,6 +4,12 @@ def calculateTime():
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
 
+    # Checks if both monkeys are smiling or if both are not smiling, else a good day
+    if monkey_one.lower() == 'y' and monkey_two.lower() == 'y' or monkey_one.lower() == 'n' and monkey_two.lower() == 'n':
+        print("Uh Oh! We're in trouble!")
+    else:
+        print("Yay! We're going to have a good day!")
+
     # end assignment
 
 
